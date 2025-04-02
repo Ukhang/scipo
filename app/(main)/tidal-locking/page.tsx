deee052326a79f0a52351efd8970d618ea7c493b
Ukhang <ukhangmarma459@gmail.com>
2025-04-02 17:08:37 +0600

    update tidal locking page

--- app/(main)/tidal-locking/page.tsx
+++ app/(main)/tidal-locking/page.tsx
@@ -1,9 +1,11 @@
 import BackToHome from "@/components/back-to-home";
+import TidalLockingSimulator from "@/components/tidal-locking-simulator";
 
 export default function TidalLockingPage() {
   return (
     <div className="min-h-screen bg-gray-950">
       <BackToHome/>
+      <TidalLockingSimulator />
     </div>
   )
 }
