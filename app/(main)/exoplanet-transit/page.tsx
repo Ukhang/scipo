9277827ef6a1fe5c14ea717412dedb4b193700d6
Ukhang <ukhangmarma459@gmail.com>
2025-04-02 14:51:47 +0600

    setup exoplanet transit visulalization page

---
+++ app/(main)/exoplanet-transit/page.tsx
@@ -0,0 +1,11 @@
+import ExoplanetTransit from "@/components/exoplanet-transit"
+
+export default function ExoplanetTransitPage() {
+  return (
+    <div className="min-h-screen bg-gray-950">
+      <BackToHome />
+      <ExoplanetTransit />
+    </div>
+  )
+}
+
