86cf307b87c8443011603ec13b2c4e1556efe5e9
Ukhang <ukhangmarma459@gmail.com>
2025-04-02 14:45:20 +0600

    update constant data

--- constants/index.ts
+++ constants/index.ts
@@ -7,4 +7,12 @@ export const simulations = [
       color: "from-purple-500 to-indigo-700",
       icon: "🪨",
     },
+    {
+      title: "Exoplanet Transit Visualization",
+      description: "See how astronomers detect planets by measuring star brightness dips.",
+      image: "/placeholder.svg?height=200&width=400",
+      href: "/exoplanet-transit",
+      color: "from-blue-500 to-cyan-700",
+      icon: "🔭",
+    },
 ]
