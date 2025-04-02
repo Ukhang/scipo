836089fede02c1b51abf7c91af61f1ff2db23d18
Ukhang <ukhangmarma459@gmail.com>
2025-04-02 17:03:24 +0600

    add TidalLockingPage

---
+++ app/(main)/tidal-locking/page.tsx
@@ -0,0 +1,10 @@
+import BackToHome from "@/components/back-to-home";
+
+export default function TidalLockingPage() {
+  return (
+    <div className="min-h-screen bg-gray-950">
+      <BackToHome/>
+    </div>
+  )
+}
+
