f9e5c6eab1de2ce7c4489386edf68f9a3902b9e1
Ukhang <ukhangmarma459@gmail.com>
2025-04-02 17:17:19 +0600

    add tidal solar eclips page

---
+++ app/(main)/solar-eclipse/page.tsx
@@ -0,0 +1,11 @@
+import BackToHome from "@/components/back-to-home";
+
+export default function SolarEclipsePage() {
+  return (
+    <div className="min-h-screen bg-black">
+      <BackToHome/>
+      
+    </div>
+  )
+}
+
