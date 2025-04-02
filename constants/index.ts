8c2e1c398d014f1e495f01e2baac4e2f559ec610
Ukhang <ukhangmarma459@gmail.com>
2025-04-02 13:48:08 +0600

    added simulation constant data

---
+++ constants/index.ts
@@ -0,0 +1,10 @@
+const simulations = [
+    {
+      title: "Asteroid Belt Simulation",
+      description: "Explore a simulated asteroid belt around a central star.",
+      image: "/placeholder.svg?height=200&width=400",
+      href: "/asteroid-belt",
+      color: "from-purple-500 to-indigo-700",
+      icon: "🪨",
+    },
+]
