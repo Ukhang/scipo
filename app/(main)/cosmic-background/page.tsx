718df56cd4824028224db8033be3fdb693ccb5df
Ukhang <ukhangmarma459@gmail.com>
2025-04-03 13:10:54 +0600

    add cosmic background page

---
+++ app/(main)/cosmic-background/page.tsx
@@ -0,0 +1,9 @@
+import BackToHome from "@/components/back-to-home";
+
+export default function CosmicBackgroundPage() {
+  return (
+    <div className="min-h-screen bg-black">
+        <BackToHome/>
+    </div>
+  )
+}
